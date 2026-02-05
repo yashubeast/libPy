60f6b22de5b0c4f15475ec72f7fa9047586c4d80
yashubeast <shenziouminami@gmail.com>
2026-02-05 22:29:33 +0530

    exclude name from logs

--- libPy/logger.py
+++ libPy/logger.py
@@ -1,14 +1,17 @@
 import logging
 import sys
 
-def get_logger(name: str):
+def new(name: str = 'app', exclude_name: bool = False):
   
   logger = logging.getLogger(name)
   logger.setLevel(logging.INFO)
 
   # format: [INFO] 29-12-2026 13:59:59: 
+  regularFormat = '[%(name)s] [%(levelname)s] [%(asctime)s]: %(message)s'
+  excludeNameFormat = '[%(levelname)s] [%(asctime)s]: %(message)s'
+  format = regularFormat if exclude_name == False else excludeNameFormat
   formatter = logging.Formatter(
-    '[%(name)s] [%(levelname)s] [%(asctime)s]: %(message)s',
+    format,
     datefmt = '%d-%m-%y %H:%M:%S'
   )
 
