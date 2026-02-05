0a616e00355e5b504c248a3e347888047af18eb0
yashubeast <shenziouminami@gmail.com>
2026-02-05 22:41:38 +0530

    colored logs

--- libPy/logger.py
+++ libPy/logger.py
@@ -1,3 +1,4 @@
+import colorlog
 import logging
 import sys
 
@@ -6,18 +7,27 @@ def new(name: str = 'app', exclude_name: bool = False):
   logger = logging.getLogger(name)
   logger.setLevel(logging.INFO)
 
-  # format: [INFO] 29-12-2026 13:59:59: 
-  regularFormat = '[%(name)s] [%(levelname)s] [%(asctime)s]: %(message)s'
-  excludeNameFormat = '[%(levelname)s] [%(asctime)s]: %(message)s'
+  # format: [app] [INFO] 29-12-2026 13:59:59: message
+  regularFormat = '%(log_color)s[%(name)s] [%(levelname)s] [%(asctime)s]: %(message)s%(reset)s'
+  # format: [INFO] 29-12-2026 13:59:59: message
+  excludeNameFormat = '%(log_color)s[%(levelname)s] [%(asctime)s]: %(message)s%(reset)s'
   format = regularFormat if exclude_name == False else excludeNameFormat
-  formatter = logging.Formatter(
+
+  formatter = colorlog.ColoredFormatter(
     format,
-    datefmt = '%d-%m-%y %H:%M:%S'
+    datefmt = '%d-%m-%Y %H:%M:%S',
+    log_colors={
+      'DEBUG': 'purple',
+      'INFO': 'green',
+      'WARNING': 'yellow',
+      'ERROR': 'red',
+      'CRITICAL': 'red, bg_white',
+    }
   )
 
+  # handler
   handler = logging.StreamHandler(sys.stdout)
   handler.setFormatter(formatter)
-
   if not logger.handlers:
     logger.addHandler(handler)
 
